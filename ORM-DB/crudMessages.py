5d734fa8daf90717920f4d9368190a14d6d30eba
Maximus <rafinhamaximus3141592@gmail.com>
2026-05-04 16:58:22 -0300

    Decoracao convertFromNick criada para facilitar a hora de juntar o banco com a rede

--- ORM-DB/crudMessages.py
+++ ORM-DB/crudMessages.py
@@ -3,8 +3,24 @@ Pequeno crud para adicionar e remover mensagens
 """
 
 from models import *
+from crudUser import (
+    readUser,
+)
 
-def createMessage(content, sender_id, reciver_id):
+def convertFromNick(func):
+    def reciveNick(nick1, nick2, argument):
+        user1 = readUser(nick1)
+        user2 = readUser(nick2)
+
+        if user1 != None and user2 != None:
+            return func(user1, user2, argument)
+        else:
+            return None
+
+    return reciveNick
+        
+@convertFromNick
+def createMessage(sender_id, reciver_id, content):
     return Messages.create(
         content=content,
         sender_id = sender_id,
@@ -24,11 +40,12 @@ def deleteChat(user_a_id, user_b_id):
     )
     return query.execute()
 
-def listHistory(user_a_id, user_b_id):
+@convertFromNick
+def listHistory(user_a_id, user_b_id, tam):
     return (Messages
             .select()
             .where(
                 ((Messages.sender_id == user_a_id) & (Messages.reciver_id == user_b_id)) |
                 ((Messages.sender_id == user_b_id) & (Messages.reciver_id == user_a_id))
             )
-            .order_by(Messages.id.asc())) # Ordena por ordem de envio
+            .order_by(Messages.id.asc()).limit(tam)) # Ordena por ordem de escrita no banco 
