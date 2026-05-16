6feaa6e64137e4a5cf5d5103f090176071e9d20d
dvdalnramalho <dvdaln.ramalho@gmail.com>
2026-05-15 21:03:25 -0300

    correçao do listen - Ref #6
    havia um problema de quebra de conexão

--- comunication.py
+++ comunication.py
@@ -1,5 +1,6 @@
+# Gerencia a conexão TCP com o servidor, enviando e recebendo pacotes JSON
+
 import socket
-import struct
 import json
 import threading
 from addres import HOST, PORT
@@ -12,31 +13,26 @@ class Comunication:
     def connect(self):
         self.chan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.chan.connect((HOST, PORT))
-        
         # Inicia uma Thread rodando em background só para escutar o servidor
         thread_escuta = threading.Thread(target=self._listen, daemon=True)
         thread_escuta.start()
 
     def _listen(self):
+        arquivo_socket = self.chan.makefile('r', encoding='utf-8')
         while True:
             try:
                 # Lê a linha JSON diretamente do servidor Java
-                arquivo_socket = self.chan.makefile('r', encoding='utf-8')
                 linha_json = arquivo_socket.readline()
-                
-                if not linha_json: break
-                
+                if not linha_json:
+                    break
                 pacote_dict = json.loads(linha_json)
-                
                 if self.on_message_callback:
                     self.on_message_callback(pacote_dict)
-                    
             except Exception as e:
                 print("Conexão perdida:", e)
                 break
-
-    def send_packet(self, packet_dict):
         # Transforma o dicionário em string JSON e envia
+    def send_packet(self, packet_dict):
         msg_json = json.dumps(packet_dict) + "\n"
         try:
             self.chan.sendall(msg_json.encode('utf-8'))
